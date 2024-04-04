9f62cd463b44c6a1d77cb7a914103436dd7c9ad7
nDjura <nemanjadjura33@gmail.com>
2024-04-04 16:55:07 +0200

    * countdown added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,17 @@
 
     @section("sadrzajStranice")
 
+        {{--            <p>Trenutno vreme je: {{ date("h:i:s") }}</p>--}}
+        <div class="container mt-4">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center bg-light p-4 rounded shadow">
+                    <h3>Do kraja kursa je ostalo</h3>
+                    <h1 id="countdown"></h1>
+                </div>
+            </div>
+        </div>
+
+        <script src="{{asset("js/countdown.js")}}"></script>
     @endsection
 </body>
 </html>
