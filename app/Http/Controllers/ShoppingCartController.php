248e72596be025cb5a49ab3abfd317539496d753
nemanjadjurovic23 <nemanjadjurovic640@gmail.com>
2024-07-01 11:48:43 +0200

    * Added logic for the cart

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CartAddRequest;
+use App\Models\ProductsModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
@@ -10,12 +11,23 @@ class ShoppingCartController extends Controller
 {
     public function index()
     {
+
+        $allProducts = array_column(Session::get('product'), 'product_id');
+        $products = ProductsModel::whereIn('id', $allProducts)->get();
+        
         return view('cart', [
             'cart' => Session::get('product'),
+            'products' => $products,
         ]);
     }
     public function addToCart(CartAddRequest $request)
     {
+        $product = ProductsModel::where(['id' => $request->id])->first();
+
+        if ($product->amount < $request->amount) {
+            return redirect()->back();
+        }
+
         Session::push('product', [
             'product_id' => $request->id,
             'amount' => $request->amount,
