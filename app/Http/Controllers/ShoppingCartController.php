7f0684c8fca6d36f70ab1c32e18ef6b46d480814
nemanjadjurovic23 <nemanjadjurovic640@gmail.com>
2024-07-01 10:00:41 +0200

    * CartAddRequest & ShoppingCartController

---
+++ app/Http/Controllers/ShoppingCartController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CartAddRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+
+class ShoppingCartController extends Controller
+{
+    public function index()
+    {
+        return view('cart', [
+            'cart' => Session::get('product'),
+        ]);
+    }
+    public function addToCart(CartAddRequest $request)
+    {
+        Session::push('product', [
+            'product_id' => $request->id,
+            'amount' => $request->amount,
+        ]);
+
+        return redirect()->route('cart.index');
+    }
+}
